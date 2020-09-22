641f71fdd9808c73ffe9b5fcac188f0d8a923e7a
Jeffrey Stoke <me@arhat.dev>
2020-09-22 22:25:20 +0200

    Update build targets

--- scripts/build/template-application-go.mk
+++ scripts/build/template-application-go.mk
@@ -88,12 +88,12 @@ template-application-go.linux.all: \
 	template-application-go.linux.mips64lehf \
 	template-application-go.linux.ppc64 \
 	template-application-go.linux.ppc64le \
-	template-application-go.linux.s390x \
-	template-application-go.linux.riscv64
+	template-application-go.linux.s390x
 
 template-application-go.darwin.amd64:
 	sh scripts/build/build.sh $@
 
+# # currently darwin/arm64 build will fail due to golang link error
 # template-application-go.darwin.arm64:
 # 	sh scripts/build/build.sh $@
 
@@ -106,9 +106,155 @@ template-application-go.windows.x86:
 template-application-go.windows.amd64:
 	sh scripts/build/build.sh $@
 
+template-application-go.windows.armv5:
+	sh scripts/build/build.sh $@
+
+template-application-go.windows.armv6:
+	sh scripts/build/build.sh $@
+
+template-application-go.windows.armv7:
+	sh scripts/build/build.sh $@
+
+# # currently no support for windows/arm64
 # template-application-go.windows.arm64:
 # 	sh scripts/build/build.sh $@
 
 template-application-go.windows.all: \
 	template-application-go.windows.x86 \
 	template-application-go.windows.amd64
+
+# # android build requires android sdk
+# template-application-go.android.amd64:
+# 	sh scripts/build/build.sh $@
+
+# template-application-go.android.x86:
+# 	sh scripts/build/build.sh $@
+
+# template-application-go.android.armv5:
+# 	sh scripts/build/build.sh $@
+
+# template-application-go.android.armv6:
+# 	sh scripts/build/build.sh $@
+
+# template-application-go.android.armv7:
+# 	sh scripts/build/build.sh $@
+
+# template-application-go.android.arm64:
+# 	sh scripts/build/build.sh $@
+
+# template-application-go.android.all: \
+# 	template-application-go.android.amd64 \
+# 	template-application-go.android.arm64 \
+# 	template-application-go.android.x86 \
+# 	template-application-go.android.armv7 \
+# 	template-application-go.android.armv5 \
+# 	template-application-go.android.armv6
+
+template-application-go.freebsd.amd64:
+	sh scripts/build/build.sh $@
+
+template-application-go.freebsd.x86:
+	sh scripts/build/build.sh $@
+
+template-application-go.freebsd.armv5:
+	sh scripts/build/build.sh $@
+
+template-application-go.freebsd.armv6:
+	sh scripts/build/build.sh $@
+
+template-application-go.freebsd.armv7:
+	sh scripts/build/build.sh $@
+
+template-application-go.freebsd.arm64:
+	sh scripts/build/build.sh $@
+
+template-application-go.freebsd.all: \
+	template-application-go.freebsd.amd64 \
+	template-application-go.freebsd.arm64 \
+	template-application-go.freebsd.armv7 \
+	template-application-go.freebsd.x86 \
+	template-application-go.freebsd.armv5 \
+	template-application-go.freebsd.armv6
+
+template-application-go.netbsd.amd64:
+	sh scripts/build/build.sh $@
+
+template-application-go.netbsd.x86:
+	sh scripts/build/build.sh $@
+
+template-application-go.netbsd.armv5:
+	sh scripts/build/build.sh $@
+
+template-application-go.netbsd.armv6:
+	sh scripts/build/build.sh $@
+
+template-application-go.netbsd.armv7:
+	sh scripts/build/build.sh $@
+
+template-application-go.netbsd.arm64:
+	sh scripts/build/build.sh $@
+
+template-application-go.netbsd.all: \
+	template-application-go.netbsd.amd64 \
+	template-application-go.netbsd.arm64 \
+	template-application-go.netbsd.armv7 \
+	template-application-go.netbsd.x86 \
+	template-application-go.netbsd.armv5 \
+	template-application-go.netbsd.armv6
+
+template-application-go.openbsd.amd64:
+	sh scripts/build/build.sh $@
+
+template-application-go.openbsd.x86:
+	sh scripts/build/build.sh $@
+
+template-application-go.openbsd.armv5:
+	sh scripts/build/build.sh $@
+
+template-application-go.openbsd.armv6:
+	sh scripts/build/build.sh $@
+
+template-application-go.openbsd.armv7:
+	sh scripts/build/build.sh $@
+
+template-application-go.openbsd.arm64:
+	sh scripts/build/build.sh $@
+
+template-application-go.openbsd.all: \
+	template-application-go.openbsd.amd64 \
+	template-application-go.openbsd.arm64 \
+	template-application-go.openbsd.armv7 \
+	template-application-go.openbsd.x86 \
+	template-application-go.openbsd.armv5 \
+	template-application-go.openbsd.armv6
+
+template-application-go.solaris.amd64:
+	sh scripts/build/build.sh $@
+
+template-application-go.aix.ppc64:
+	sh scripts/build/build.sh $@
+
+template-application-go.dragonfly.amd64:
+	sh scripts/build/build.sh $@
+
+template-application-go.plan9.amd64:
+	sh scripts/build/build.sh $@
+
+template-application-go.plan9.x86:
+	sh scripts/build/build.sh $@
+
+template-application-go.plan9.armv5:
+	sh scripts/build/build.sh $@
+
+template-application-go.plan9.armv6:
+	sh scripts/build/build.sh $@
+
+template-application-go.plan9.armv7:
+	sh scripts/build/build.sh $@
+
+template-application-go.plan9.all: \
+	template-application-go.plan9.amd64 \
+	template-application-go.plan9.armv7 \
+	template-application-go.plan9.x86 \
+	template-application-go.plan9.armv5 \
+	template-application-go.plan9.armv6
